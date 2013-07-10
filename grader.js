c4957b41bc3bcef483a5b2e4a1add46e9b51bfe8
Vidhya Gholkar <me@server.fake>
2013-07-10 23:57:14 +0000

    added url input capability

--- grader.js
+++ grader.js
@@ -23,10 +23,12 @@ References:
 
 var fs = require('fs');
 var program = require('commander');
+var sys = require('util');
 var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
-var CHECKSFILE_DEFAULT = "checks.json";
-
+var rest = require('restler');
+// var HTMLFILE_DEFAULT = "index.html";
+// var CHECKSFILE_DEFAULT = "checks.json";
+// var URL_DEFAULT = "http://github.com";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -44,16 +46,15 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var  checkHtmlFile = function($, checksfile){
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
-    }
-    return out;
-};
+}
+return out
+}
 
 var clone = function(fn) {
     // Workaround for commander.js issue.
@@ -63,12 +64,28 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists))
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists))
+.option('-u, --url <pulls_file>','Pulls file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+
+if (program.file){
+ $ = cheerioHtmlFile(program.file)
+var checkJson = checkHtmlFile($, program.checks)
+var outJson = JSON.stringify(checkJson, null, 4);
+console.log(outJson);
+}
+if (program.url){
+rest.get(program.url).on('complete',function(result){
+$=cheerio.load(result);
+var checkJson = checkHtmlFile($, program.checks)
+var outJson = JSON.stringify(checkJson, null, 4);
+console.log(outJson)
+})
+
+
+}
+} else
+ {
     exports.checkHtmlFile = checkHtmlFile;
 }
