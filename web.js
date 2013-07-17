b42cb4e58767c627a111de822df3ef0fc6bf59ce
Vidhya Gholkar <me@server.fake>
2013-07-17 23:56:20 +0000

    web.js port change, index.html improved

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ buffer = new Buffer(fs.readFileSync('index.html','utf-8'))
 response.send(buffer.toString())
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
